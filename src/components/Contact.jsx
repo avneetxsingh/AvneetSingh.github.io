3605dcb826127d91b739706cdfc8800af1042381
Avneet Singh <113480082+avneetxsingh@users.noreply.github.com>
2023-12-29 16:32:44 -0500

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,7 +37,7 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Avneet Singh",
           from_email: form.email,
           to_email: "anln8@rockets.utoledo.edu",
           message: form.message,
